9071f5588add73393dd1d67376ca45e501188a2b
symphonyh <cloudhan@Thinkpad-Cloudhan.localdomain>
2022-10-28 15:47:49 +0800

    use boostrap

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # get 'root/home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to:'root#home'
+end
