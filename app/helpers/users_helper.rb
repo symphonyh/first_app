c862ce053c82cee985af32e38f7e71cb352e851a
symphonyh <cloudhan@Thinkpad-Cloudhan.localdomain>
2022-11-11 14:56:22 +0800

    use following-last

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-    def gravatar_for(user, options = { size: 150 })
+    def gravatar_for(user, options = { size: 75 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://cravatar.cn/avatar/#{gravatar_id}?s=#{options[:size]}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
